6c5328be431eaff2996ff8345ff09e04e1c1bb6e
GuessMF <96071469+GuessMF@users.noreply.github.com>
2023-05-18 11:39:01 +0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,11 +4,29 @@ let menu = document.querySelector(".menu");
 
 menuBtn.addEventListener("click", () => {
   menu.classList.toggle("active");
-  rule.forEach((elem) => {
-    elem.classList.toggle("active");
-  });
-
-  menu.classList[1] == "active"
-    ? (document.body.style.position = "fixed")
-    : (document.body.style.position = "static");
+  if (menu.classList[1] == "active") {
+    document.body.style.position = "fixed";
+    openedMenu();
+    rule.forEach((elem) => {
+      elem.classList.add("active");
+    });
+  } else {
+    document.body.style.position = "static";
+    rule.forEach((elem) => {
+      elem.classList.remove("active");
+    });
+  }
 });
+
+const openedMenu = () => {
+  menu.addEventListener("click", (event) => {
+    console.log(event.target.tagName);
+    if (event.target.tagName === "A" || event.target.tagName === "BUTTON") {
+      menu.classList.remove("active");
+      rule.forEach((elem) => {
+        elem.classList.remove("active");
+      });
+      document.body.style.position = "static";
+    }
+  });
+};
